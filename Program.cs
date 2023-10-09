9d4fe0aa717fb3eda01c3d30fe5494cdc786ebbb
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-09 11:05:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration
+            string name="John";
+            int age=35;
+            bool isTall=false;
+            double weight = 80.2;
+
+            /*for the smallest least precise decimal use float,
+             * * for more precise use decimal eg for money'
+             * * in most cases  duble will do just fine
+             * * */
+
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
